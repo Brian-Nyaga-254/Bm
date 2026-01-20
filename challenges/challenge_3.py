ea382b3e23476b0f809dc8111f6ed61809d65b37
Brian-Nyaga-254 <brianmugai255@gmail.com>
2026-01-20 06:24:34 +0300

    Update challenge_3.py

--- challenges/challenge_3.py
+++ challenges/challenge_3.py
@@ -2,11 +2,11 @@
 Ask the user for their name
 Remove leading and trailing spaces
 Capitalize the name
-Print exactly this output format:
 """
 name = input("What is your name? ")
 name = name.strip()
 name = name.capitalize()
 print(f"Hello {name}", end = "\n")
 print("Welcome")
+
 
