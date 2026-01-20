3b6b016fb9d7a50da1c32fe9561b35bf31170f02
Brian-Nyaga-254 <brianmugai255@gmail.com>
2026-01-20 06:17:19 +0300

    Update strings_strip_capitalize.py

--- strings/strings_strip_capitalize.py
+++ strings/strings_strip_capitalize.py
@@ -1,3 +1,7 @@
+"""
+A program that removes whitespaces 
+And capitalizes users input
+"""
 #Ask user to input their name
 name = input('What is your name? ')
 #Greet the user with their name
@@ -5,4 +9,5 @@ name = name.strip()
 name = name.capitalize()
 
 print('Hello,' , name , end = '',)
+
 
