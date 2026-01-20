37da54610e57188133aadd2c529d9dac88b18381
Brian-Nyaga-254 <brianmugai255@gmail.com>
2026-01-20 06:20:55 +0300

    Update Challenge_1.py

--- challenges/Challenge_1.py
+++ challenges/Challenge_1.py
@@ -1,3 +1,14 @@
+"""
+Problem: Smart Greeting Program (Version 1)
+Build a Python program that does ALL of the following using ONLY what you already know:
+Requirements
+Ask the user for their full name
+Clean the input:
+Remove leading/trailing spaces
+Format the name:
+Capitalize it properly
+Ask the user how they are feeling today (one word)
+"""
 name = input('What is your name: ')
 name = name.strip()
 name = name.capitalize()
@@ -5,3 +16,4 @@ mood = input("How are you feeling today: ")
 mood = mood.strip()
 mood = mood.capitalize()
 print(f"Hello {name} I see you are feeling {mood} today")
+
